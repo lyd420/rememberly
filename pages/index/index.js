5653037e468b2a04a4fe0e8297c26c00421918c0
lyd240 <lyd4201992@gmail.com>
2018-09-14 17:45:52 +0800

    20180914 patch

--- pages/index/index.js
+++ pages/index/index.js
@@ -84,5 +84,17 @@ Page({
         })
       },
     })
+    try {
+      var res = wx.getSystemInfoSync()
+      console.log(res.model)
+      console.log(res.pixelRatio)
+      console.log(res.windowWidth)
+      console.log(res.windowHeight)
+      console.log(res.language)
+      console.log(res.version)
+      console.log(res.platform)
+    } catch (e) {
+      // Do something when catch error
+    }
   }
 })
